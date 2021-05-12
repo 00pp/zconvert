5c09a56c1ce737a6f0cc99ac75748729daacb057
Dastan Turysbekov <uitlaber@gmail.com>
2021-05-12 18:19:36 +0600

    Title content в конфиги

--- config/convertor.php
+++ config/convertor.php
@@ -9,5 +9,15 @@ return [
             'rules' => 'mimes:doc,docx',
             'mimes' => 'application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document' 
         ],
+    ],
+    'content' => [
+        'docx-to-pdf' => [
+            'title' => 'Title for docx-to-pdf',
+            'footer' => 'Footer text for docx-to-pdf'
+        ],
+        'docx-to-jpg' => [
+            'title' => 'Title for docx-to-jpg',
+            'footer' => 'Footer text for docx-to-jpg'
+        ]
     ]
 ];
--- resources/views/convertor/page.blade.php
+++ resources/views/convertor/page.blade.php
@@ -1,16 +1,18 @@
 @extends('layouts.frontend')
 
 @section('title')
-    File Converter {{$type}}
+    {{config('convertor.content.'.$type.'.title')}}
 @endsection
 @section('content')
+
+
 
   <div class=" text-gray-200 bg-gray-900">
         <div class="max-w-6xl mx-auto px-5 py-24 ">
 
         <div class="flex flex-wrap w-full  flex-col items-center text-center">
                     <h1 class=" title-font mb-2 text-4xl font-extrabold leading-10 tracking-tight text-left sm:text-5xl sm:leading-none md:text-6xl">
-                        File Converter {{$type}}</h1>
+                        {{config('convertor.content.'.$type.'.title')}}</h1>
                     <p class="lg:w-1/2 w-full leading-relaxed text-base">
                         ConvertApp is an online file converter. Right now support Docx and Doc conversion to image
                         format. To get started, use the button below and select files to convert from your computer.
@@ -28,5 +30,5 @@
 @endsection
 
 @section('footer')
-   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, tempora.</p>
+   <p>{{config('convertor.content.'.$type.'.footer')}}</p>
 @endsection
